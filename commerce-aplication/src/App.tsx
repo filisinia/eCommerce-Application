856eb8d960517395449ebf1fcab06b3f3838a23a
Mykhailo Nikolaiev <ab3mn3@gmail.com>
2024-05-03 06:23:29 +0300

    feat:add materialUI components,fonts,icons

--- commerce-aplication/src/App.tsx
+++ commerce-aplication/src/App.tsx
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.scss';
+import Button from '@mui/material/Button';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const handleSetCount = () => {
+    setCount(count + 1);
+    console.log(count);
+  };
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Button variant='contained' onClick={handleSetCount}>
+      Hello world
+    </Button>
   );
 }
 
