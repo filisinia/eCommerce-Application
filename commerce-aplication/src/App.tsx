58d6bfad77599082663061039b20b2d0c9bc1d9f
Mykhailo Nikolaiev <ab3mn3@gmail.com>
2024-05-05 06:53:44 +0300

    feat:add prettier and eslint configurations,also add scripts to its

--- commerce-aplication/src/App.tsx
+++ commerce-aplication/src/App.tsx
@@ -1,27 +1,23 @@
-import Button from '@mui/material/Button';
 import { useState } from 'react';
-import './styles/styles.scss';
 
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
+import Button from '@mui/material/Button';
 
-function App() {
+import './styles/styles.scss';
+
+const App = (): JSX.Element => {
   const [count, setCount] = useState(0);
 
-  const handleSetCount = () => {
+  const handleSetCount = (): void => {
     setCount(count + 1);
-    console.log(count);
   };
 
   return (
-    <>
-      {' '}
-      <Button variant='contained' onClick={handleSetCount}>
-        <AccessAlarmIcon />
-        <span className='test'> Hello world</span>
-      </Button>
-    </>
+    <Button variant='contained' onClick={handleSetCount}>
+      <AccessAlarmIcon />
+      <span className='test'> Hello world</span>
+    </Button>
   );
-}
+};
 
 export default App;
-
