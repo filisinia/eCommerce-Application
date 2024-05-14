7bc01d17a2c10329084bf813577047c5d644bdd7
Mariia Petrenko <filisinia@gmail.com>
2024-05-14 17:13:51 +0300

    feat: implement routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import Header from 'components/header/Header';
 import AuthCustomerPage from 'pages/AuthCustomerPage/AuthCustomerPage';
+import MainPage from 'pages/MainPage/MainPage';
 
 const App = (): JSX.Element => (
-  <main>
+  <BrowserRouter>
     <Header />
-    <AuthCustomerPage />
-  </main>
+    <Routes>
+      <Route element={<MainPage />} path='/' />
+      <Route element={<AuthCustomerPage />} path='/signup' />
+    </Routes>
+  </BrowserRouter>
 );
 
 export default App;
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,16 +1,21 @@
-import { AppBar, Button, Toolbar, Typography } from '@mui/material';
+import { AppBar, Button, Toolbar, Box } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const Header = (): JSX.Element => (
   <>
     <AppBar sx={{ backgroundColor: '#0d0d0d' }}>
       <Toolbar>
-        <Typography variant='h5' sx={{ flexGrow: 1 }}>
-          Store Name
-        </Typography>
-        <Button variant='outlined' sx={{ marginRight: '10px' }}>
+        <Box sx={{ flexGrow: 1 }}>
+          <Button component={Link} to='/'>
+            Store Name
+          </Button>
+        </Box>
+        <Button component={Link} to='/login' variant='outlined' sx={{ marginRight: '10px' }}>
           Log in
         </Button>
-        <Button variant='contained'>Sign up</Button>
+        <Button component={Link} to='/signup' variant='contained'>
+          Sign up
+        </Button>
       </Toolbar>
     </AppBar>
     <Toolbar />
