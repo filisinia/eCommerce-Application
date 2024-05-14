2f54f55d53074ce5af42b0744ec81945c459090e
Mariia Petrenko <filisinia@gmail.com>
2024-05-14 15:33:07 +0300

    refactor: remove styles folder and add normalize

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import Header from 'components/header/Header';
 import AuthCustomerPage from 'pages/AuthCustomerPage/AuthCustomerPage';
 
-import 'styles/styles.scss';
-
 const App = (): JSX.Element => (
   <main>
     <Header />
