83fc8f117d978d86a970c1d5615417d111d22f17
Reel <ladias231299@gmail.com>
2024-05-18 19:49:30 +0300

    feat: add password showButton

--- src/components/customer/LoginCustomer.tsx
+++ src/components/customer/LoginCustomer.tsx
@@ -1,18 +1,55 @@
-import { Box, Typography, TextField, Button } from '@mui/material';
+import { useState } from 'react';
+
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+import { Box, Typography, TextField, Button, IconButton, InputAdornment } from '@mui/material';
 
 import styles from 'components/customer/AuthCustomerStyle';
 
 const LoginCustomer = (): JSX.Element => {
-  const asd = '';
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  const handleClickShowPassword = (): void => {
+    setShowPassword((prev: boolean) => !prev);
+  };
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+  };
+
+  const onChange = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+  };
 
   return (
     <Box>
       <Typography component='h1' variant='h3' sx={{ textAlign: 'center' }}>
         Log in
       </Typography>
-      <Box component='form' sx={styles.formStyle}>
-        <TextField label='Login' name='login' autoFocus size='small' type='login' />
-        <TextField label='Password' name='password' autoFocus size='small' type='password' />
+      <Box component='form' onSubmit={onSubmit} onChange={onChange} sx={styles.formStyle}>
+        <TextField
+          label='Email adress'
+          name='email'
+          autoFocus
+          size='small'
+          type='email'
+          sx={{ mb: 2, width: '230px' }}
+        />
+        <TextField
+          label='Password'
+          name='password'
+          size='small'
+          type={showPassword ? 'text' : 'password'}
+          sx={{ mb: 2, width: '230px' }}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position='end'>
+                <IconButton aria-label='toggle password visibility' onClick={handleClickShowPassword} edge='end'>
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
         <Button type='submit'>Log in</Button>
       </Box>
     </Box>
