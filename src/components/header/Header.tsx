d4c3d192183ed118d2c8176f70a971f2e99f94e0
Mariia Petrenko <filisinia@gmail.com>
2024-05-20 16:12:48 +0300

    feat: implement logout

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,25 +1,36 @@
 import { AppBar, Button, Toolbar, Box } from '@mui/material';
 import { Link } from 'react-router-dom';
 
-const Header = (): JSX.Element => (
-  <>
-    <AppBar sx={{ backgroundColor: '#0d0d0d' }}>
-      <Toolbar>
-        <Box sx={{ flexGrow: 1 }}>
-          <Button component={Link} to='/'>
-            Store Name
+import authCustomerStore from 'store/slices/customer/authCustomerSlice';
+
+const Header = (): JSX.Element => {
+  const { setCustomer } = authCustomerStore((state) => state);
+
+  const onLogout = (): void => setCustomer(null);
+
+  return (
+    <>
+      <AppBar sx={{ backgroundColor: '#0d0d0d' }}>
+        <Toolbar>
+          <Box sx={{ flexGrow: 1 }}>
+            <Button component={Link} to='/'>
+              Store Name
+            </Button>
+          </Box>
+          <Button component={Link} to='/login' variant='outlined' sx={{ marginRight: '10px' }}>
+            Log in
           </Button>
-        </Box>
-        <Button component={Link} to='/login' variant='outlined' sx={{ marginRight: '10px' }}>
-          Log in
-        </Button>
-        <Button component={Link} to='/signup' variant='contained'>
-          Sign up
-        </Button>
-      </Toolbar>
-    </AppBar>
-    <Toolbar />
-  </>
-);
+          <Button component={Link} to='/signup' variant='contained' sx={{ marginRight: '10px' }}>
+            Sign up
+          </Button>
+          <Button variant='outlined' onClick={onLogout}>
+            Log out
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+    </>
+  );
+};
 
 export default Header;
