575434e4312aae8633b2a8d7af30d5a311690aba
Mariia Petrenko <filisinia@gmail.com>
2024-05-29 19:20:08 +0300

    refactor: move data into const variables

--- src/components/productInfo/ProductDescription.tsx
+++ src/components/productInfo/ProductDescription.tsx
@@ -4,12 +4,13 @@ import ProductInfoStyle from 'components/productInfo/ProductInfoStyle';
 import { IProduct } from 'types/products';
 
 const ProductDescription = ({ productInfo }: { productInfo: IProduct }): JSX.Element => {
+  const { name, description } = productInfo?.masterData?.current || {};
   const priceData = productInfo?.masterData?.current?.masterVariant?.prices?.[0];
 
   return (
     <Box sx={ProductInfoStyle.productDescription.descriptionBox}>
       <Typography component='h2' variant='h4' sx={ProductInfoStyle.productDescription.name}>
-        {productInfo?.masterData?.current?.name?.['en-US']}
+        {name['en-US']}
       </Typography>
       <Box sx={ProductInfoStyle.productDescription.priceBox}>
         <Typography sx={ProductInfoStyle.productDescription.currentPrice}>
@@ -19,7 +20,7 @@ const ProductDescription = ({ productInfo }: { productInfo: IProduct }): JSX.Ele
           {priceData?.discounted?.value?.centAmount.toLocaleString()}
         </Typography>
       </Box>
-      <Typography>{productInfo?.masterData?.current?.description?.['en-US']}</Typography>
+      <Typography>{description['en-US']}</Typography>
     </Box>
   );
 };
--- src/components/productInfo/ProductImages.tsx
+++ src/components/productInfo/ProductImages.tsx
@@ -4,29 +4,24 @@ import ProductInfoStyle from 'components/productInfo/ProductInfoStyle';
 import { IProduct } from 'types/products';
 
 const ProductImages = ({ productInfo }: { productInfo: IProduct }): JSX.Element => {
+  const { name } = productInfo.masterData.current || {};
   const imagesData = productInfo?.masterData.current.masterVariant.images;
   const imagesQuantity = imagesData.length;
 
   const mainImage = (
-    <img
-      src={imagesData[0].url}
-      style={ProductInfoStyle.productImages.mainImage}
-      alt={productInfo.masterData.current.name['en-US']}
-    />
+    <img src={imagesData[0].url} style={ProductInfoStyle.productImages.mainImage} alt={name['en-US']} />
   );
 
-  const images = imagesData.map((imageData, index) =>
-    index === 0 ? null : (
-      <ImageListItem key={imageData.url} sx={ProductInfoStyle.productImages.imageItem}>
-        <img src={imageData.url} alt={productInfo.masterData.current.name['en-US']} />
-      </ImageListItem>
-    ),
-  );
+  const images = imagesData.map((imageData) => (
+    <ImageListItem key={imageData.url} sx={ProductInfoStyle.productImages.imageItem}>
+      <img src={imageData.url} alt={name['en-US']} />
+    </ImageListItem>
+  ));
 
   return (
     <>
       {mainImage}
-      <ImageList cols={imagesQuantity - 1} gap={10} sx={ProductInfoStyle.productImages.imageList}>
+      <ImageList cols={imagesQuantity} gap={10} sx={ProductInfoStyle.productImages.imageList}>
         {images}
       </ImageList>
     </>
