0feefff8a3594ae2efdd3953bec9384bb9eba966
Mariia Petrenko <filisinia@gmail.com>
2024-06-03 21:55:31 +0300

    refactor: remove client file

--- src/api/client.ts
+++
@@ -1,9 +0,0 @@
-import { createApiBuilderFromCtpClient } from '@commercetools/platform-sdk';
-
-import { ctpClient } from 'api/clientBuilder';
-
-const apiRoot = createApiBuilderFromCtpClient(ctpClient).withProjectKey({
-  projectKey: process.env.REACT_APP_API_PROJECT_KEY || '',
-});
-
-const getProject = (): Promise<unknown> => apiRoot.get().execute();
