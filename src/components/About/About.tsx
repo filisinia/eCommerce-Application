0afc9a61a72d7612b6a32bf643ae1e8137b1a7ba
Nikolaiev Mykhailo <ab3mn3@gmail.com>
2024-06-18 06:18:04 +0300

    refactor:add contributions to team

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -13,7 +13,17 @@ const team = [
     role: 'Team lead',
     git: 'https://github.com/filisinia',
     bio: `I'm a front-end developer specializing in building web applications with React and TypeScript. I focus on delivering high-quality user experiences and writing clean, maintainable code. My goal is to achieve efficiency and excellence in every project, creating modern and visually appealing web solutions`,
-    contributions: [],
+    contributions: [
+      { text: 'Repository Setup', id: 0 },
+      { text: 'Task Board Setup', id: 1 },
+      { text: 'Main Page Enhancements', id: 2 },
+      { text: 'Routing Implementation', id: 3 },
+      { text: 'Product Filtering and Searching', id: 4 },
+      { text: 'Category Navigation', id: 5 },
+      { text: 'Product Page', id: 6 },
+      { text: 'Catalog Page', id: 7 },
+      { text: 'Shopping Cart Integration', id: 8 },
+    ],
   },
   {
     id: 1,
@@ -23,7 +33,17 @@ const team = [
     role: 'Developer',
     git: 'https://github.com/ab3mn',
     bio: `I'm a junior front-end developer`,
-    contributions: [],
+    contributions: [
+      { text: 'Development Environment Configuration', id: 0 },
+      { text: 'Development Scripts ', id: 1 },
+      { text: 'Registration Page', id: 2 },
+      { text: 'Product List and sorting', id: 3 },
+      { text: 'Interactive Product Cards ', id: 4 },
+      { text: 'Profile Page', id: 5 },
+      { text: 'Routing Implementation', id: 6 },
+      { text: 'Basket Page', id: 7 },
+      { text: 'About Us Page', id: 8 },
+    ],
   },
   {
     id: 2,
@@ -33,7 +53,11 @@ const team = [
     role: 'Developer',
     git: 'https://github.com/reel-q',
     bio: `I'm a junior front-end developer`,
-    contributions: [],
+    contributions: [
+      { text: 'CommerceTools Project and API Client Setup', id: 0 },
+      { text: 'Comprehensive README', id: 1 },
+      { text: 'Login Page', id: 2 },
+    ],
   },
 ];
 
